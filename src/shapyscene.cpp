2ffe2b3e83bbb55e8337367de311c9d5230f9d36
Ryumin Sergey <90983639+ryu-min@users.noreply.github.com>
2023-09-21 21:38:10 +0300

    add src folder

---
+++ src/shapyscene.cpp
@@ -0,0 +1,6 @@
+#include "shapyscene.h"
+
+ShapyScene::ShapyScene()
+{
+
+}
---
+++ src/shapyscene.h
@@ -0,0 +1,11 @@
+#ifndef SHAPYSCENE_H
+#define SHAPYSCENE_H
+
+
+class ShapyScene
+{
+public:
+    ShapyScene();
+};
+
+#endif // SHAPYSCENE_H
